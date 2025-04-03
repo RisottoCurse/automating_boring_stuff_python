2e84e77fdf46ab7e8993b850111683dce976c188
Ashutosh Madhu <ash@Ashutoshs-MacBook-Air.local>
2025-04-03 18:04:09 +0100

    manual checking of us number

---
+++ pattern_matching_regex/isPhoneNumber.py
@@ -0,0 +1,25 @@
+def main(): 
+    number = isPhoneNumber(input('Enter a US phone number: '))
+    print(f"{number}")
+
+
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+        if text[3] != '-':
+            return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+        if text[7] != '-':
+            return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+
+main()
